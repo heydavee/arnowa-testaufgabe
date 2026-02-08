eed2010f69fe9022b89888b0967ba6c3e9534a9d
David Woodward <coding@heydave.de>
2026-02-08 10:12:41 +0100

    feat: extend ProductComplianceInfoEntity with getters and setters

--- ArnowaProductComplianceInfo/src/Core/Content/ProductComplianceInfo/ProductComplianceInfoEntity.php
+++ ArnowaProductComplianceInfo/src/Core/Content/ProductComplianceInfo/ProductComplianceInfoEntity.php
@@ -16,4 +16,44 @@ class ProductComplianceInfoEntity extends Entity
   protected bool $complianceRequired = false;
   protected ?string $complianceText = null;
   protected ?ProductEntity $product = null;
+
+  public function getProductId(): string
+  {
+    return $this->productId;
+  }
+
+  public function setProductId(string $productId): void
+  {
+    $this->productId = $productId;
+  }
+
+  public function getComplianceRequired(): bool
+  {
+    return $this->complianceRequired;
+  }
+
+  public function setComplianceRequired(bool $complianceRequired): void
+  {
+    $this->complianceRequired = $complianceRequired;
+  }
+
+  public function getComplianceText(): ?string
+  {
+    return $this->complianceText;
+  }
+
+  public function setComplianceText(?string $complianceText): void
+  {
+    $this->complianceText = $complianceText;
+  }
+
+  public function getProduct(): ?ProductEntity
+  {
+    return $this->product;
+  }
+
+  public function setProduct(?ProductEntity $product): void
+  {
+    $this->product = $product;
+  }
 }
